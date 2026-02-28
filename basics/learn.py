16cb790156ba31f28327967bf71b7fdd369c6dc9
Sardar-Sadiq <sardarsadiq106@gmail.com>
2026-02-28 14:37:31 +0530

    learning python for flask

---
+++ basics/learn.py
@@ -0,0 +1,32 @@
+# python learn.py
+# name = "sadiq"
+# age = 22
+# print(name, age)
+
+# data types
+# String 
+# integer
+# float
+# boolean
+# list
+# dictionary
+
+
+#funtions
+# def greet(name):
+#     return "hello " + name
+# print(greet("sadiq"))
+
+#if condition 
+# age = 22
+# if age < 18:
+#     print("your are a lil brat boiiiii")
+# else:
+#     print("welcome to the reality check boiiii")
+
+
+
+#loops 
+# n = 5
+# for i in range(n):
+#     print(i)
