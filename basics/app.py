3bf888296c1a4f5465166a7b790dd4e81a788f50
Sardar-Sadiq <sardarsadiq106@gmail.com>
2026-02-28 15:18:26 +0530

    python foundations

--- basics/app.py
+++ basics/app.py
@@ -1,11 +1,11 @@
-from flask import Flask  # importing the flask module
+from flask import Flask  # importing the flask module.
 
-app = Flask(__name__) # creating an instance of th flask class
+app = Flask(__name__) # creating an instance of th flask class.
 
-@app.route("/") # defining a route for the home page
+@app.route("/") # defining a route for the home page.
 
 def home():
-    return "task flow backedn running!!!" #retruning a response when the home page is acces
+    return "task flow backedn running!!!" #retruning a response when the home page is access.
 
 if __name__ == "__main__":
-    app.run(debug=True) #running the flask application in debug mode
+    app.run(debug=True) #running the flask application in debug mode.
