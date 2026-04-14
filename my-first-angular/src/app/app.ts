7caffbc6286f58b0cd39894bb743856532f6c6c5
Sardar-Sadiq <sardarsadiq106@gmail.com>
2026-04-14 12:56:27 +0530

    working on angular

--- my-first-angular/src/app/app.ts
+++ my-first-angular/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { CoursesComponent } from './courses.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CoursesComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-first-angular');
+  protected readonly title = signal('Angular app');
 }
